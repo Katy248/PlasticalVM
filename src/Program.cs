e4d2eff01554e3c732115d414b690bb0f0e2cae3
Katy248 <petrovanton247@gmail.com>
2022-08-22 15:33:22 +0300

    Убраны лишние комментарии

--- src/Program.cs
+++ src/Program.cs
@@ -16,25 +16,9 @@ internal class Program
         Console.Title = "Plastical";
         PlasticalRunner.Translators.Add(".fpl", new FunctionalPlasticalLanguage());
 
-        args = new string[] { "tests/plastical-hello-world.fpl","run" };
+        //args = new string[] { "tests/plastical-hello-world.fpl","run" };
 
         new ConsoleParser.Parser().Parse(args);
-
-        /*if (args.Length == 0) return;
-        if (!string.IsNullOrEmpty(args[0]))
-        {
-            try
-            {
-                var vmCode2 = PlasticalRunner.GetVMCode(args[0]);
-
-                PlasticalRunner.Run(vmCode2);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-        //tests.LangTest.RunTest();*/
         Console.ReadKey();
     }
 }
