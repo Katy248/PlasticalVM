ebaa27766932d67778e894cf2cb0df0fbec80dfb
Katy248 <petrovanton247@gmail.com>
2022-08-22 15:33:01 +0300

    Добавлен запуск парсера

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,5 @@
-﻿using VirtualMachine;
-using FPL.Structures;
-using FPL;
+﻿using FPL;
 using LanguageInterfaces;
-namespace PlasticalVM;
-
 
 /* 
  * Комманды:
@@ -12,7 +8,7 @@ namespace PlasticalVM;
  *  MakeVmFile + not vm sourse file path
  *  UpdateVmFile + vm sourse file (optimize sourse file)
  */
-
+namespace PlasticalVM;
 internal class Program
 {
     static void Main(string[] args)
@@ -20,21 +16,25 @@ internal class Program
         Console.Title = "Plastical";
         PlasticalRunner.Translators.Add(".fpl", new FunctionalPlasticalLanguage());
 
-        if (args.Length == 0) return;
+        args = new string[] { "tests/plastical-hello-world.fpl","run" };
+
+        new ConsoleParser.Parser().Parse(args);
+
+        /*if (args.Length == 0) return;
         if (!string.IsNullOrEmpty(args[0]))
         {
             try
             {
                 var vmCode2 = PlasticalRunner.GetVMCode(args[0]);
 
-                VM.Run(vmCode2);
+                PlasticalRunner.Run(vmCode2);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
-        //tests.LangTest.RunTest();
+        //tests.LangTest.RunTest();*/
         Console.ReadKey();
     }
 }
