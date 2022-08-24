5159d29db73c385dbb7fd4b9184d7d52aace2b8b
Katy248 <petrovanton247@gmail.com>
2022-08-24 16:19:42 +0300

    Добавлен файл

---
+++ src/ConsoleOptions/Options.cs
@@ -0,0 +1,41 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace ConsoleOptions;
+
+[Verb("run", HelpText="Execute file in specified directory.")]
+public class RunOptions
+{
+    [Value(1,Required = true, HelpText = "Path to project directory.", MetaName ="directoryPath")]
+    public string? DirectoryPath { get; set; }
+    public static int Action(RunOptions option)
+    {
+        if (Directory.Exists(option.DirectoryPath))
+        {
+            try
+            {
+                LanguageInterfaces.PlasticalRunner.Run(option.DirectoryPath);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+        else
+        {
+            Console.Error.WriteLine($"Directory \"{option.DirectoryPath}\" was not found.");
+        }
+        return -1;
+    }
+}
+[Verb("runfile", HelpText = "Execute file.")]
+public class RunFileOptions
+{
+    [Value(1, Required = true, HelpText = "Path to file.", MetaName = "filePath")]
+    public string? FilePath { get; set; }
+    public static int Action(RunFileOptions option)
+    {
+        return 0;
+    }
+}
